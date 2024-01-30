e020e2364cea4301096f0bea152fd17659034bbe
vlo2002 <69879770+vlo2002@users.noreply.github.com>
2024-01-30 15:46:22 -0600

    Updated add_feature branch

--- test_ino/test_ino.ino
+++ test_ino/test_ino.ino
@@ -3,10 +3,13 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   // put your setup code here, to run once:
-
+  lcd.begin(16, 2); //Initiate the LCD in a 16x2 configuration
+  lcd.print("My name is:")
+  lcd.setCursor(0, 1);
+  lcd.print("[Your name here!]")
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  delay(50) // wait 50ms
 }
